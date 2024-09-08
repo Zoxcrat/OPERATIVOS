d388081facd6360469db730c02c6da5601e817a7
LautaroRomero21 <lautaroromerog021@gmail.com>
2024-09-08 00:56:54 +0000

    plani corto plazo a priori hecho (sin testear)

--- kernel/src/plani_corto_plazo.h
+++ kernel/src/plani_corto_plazo.h
@@ -13,6 +13,7 @@ void procesar_motivo_devolucion(TCB* hilo, motivo_devolucion motivo);
 void agregar_a_ready(TCB* tcb);
 t_cola_multinivel* obtener_o_crear_cola_con_prioridad(int prioridad_buscada);
 void verificar_eliminacion_cola_multinivel(int prioridad_buscada);
+t_cola_multinivel* obtener_cola_con_mayor_prioridad();
 
 
 #endif //KERNEL_PLANI_CORTO_PLAZO_H
